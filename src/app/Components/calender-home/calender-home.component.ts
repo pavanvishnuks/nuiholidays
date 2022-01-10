8464deacdb000d4aa5bfc36c1e45c20434ec4953
pavanvishnuks <pvishnuks@gmail.com>
2022-01-10 13:10:34 +0530

    code updates

--- src/app/Components/calender-home/calender-home.component.ts
+++ src/app/Components/calender-home/calender-home.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from "@angular/core";
-import { AngularmaterialModule } from "src/app/Modules/angularmaterial/angularmaterial.module";
-import { Router } from "@angular/router";
-import { MatDialog } from "@angular/material/dialog";
-import { DatePipe } from "@angular/common";
-import { formatDate } from "@angular/common";
-import { MediaObserver, MediaChange } from "@angular/flex-layout";
-import { Subscription } from "rxjs";
-import { CalendarService } from "src/app/Services/CalendarService";
+import { Component, OnInit } from '@angular/core';
+import { AngularmaterialModule } from 'src/app/Modules/angularmaterial/angularmaterial.module';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { DatePipe } from '@angular/common';
+import { formatDate } from '@angular/common';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
+import { CalendarService } from 'src/app/Services/CalendarService';
 export type upcomingEvents = {
   ename: String;
   eimg: String;
@@ -16,20 +16,20 @@ export type HolidayPage = {
   name: string;
   desc: string;
   img: string;
-  employees: [upcomingEvents];
+  Uevents: [upcomingEvents];
 };
 @Component({
-  selector: "app-calender-home",
-  templateUrl: "./calender-home.component.html",
-  styleUrls: ["./calender-home.component.css"],
+  selector: 'app-calender-home',
+  templateUrl: './calender-home.component.html',
+  styleUrls: ['./calender-home.component.css'],
 })
 export class CalenderHomeComponent implements OnInit {
   public value: Date[] = [
-    new Date("05/16/2017"),
-    new Date("05/12/2017"),
-    new Date("05/6/2017"),
-    new Date("05/26/2017"),
-    new Date("05/20/2017"),
+    new Date('05/16/2017'),
+    new Date('05/12/2017'),
+    new Date('05/6/2017'),
+    new Date('05/26/2017'),
+    new Date('05/20/2017'),
   ];
   public multiselect: Boolean = true;
 
@@ -39,7 +39,7 @@ export class CalenderHomeComponent implements OnInit {
   constructor(public datepipe: DatePipe) {
     this.currentDateTime = this.datepipe.transform(
       new Date(),
-      "EEEE, MMMM d, y"
+      'EEEE, MMMM d, y'
     );
     console.log(this.currentDateTime);
   }
--- src/app/Services/CalendarService.ts
+++ src/app/Services/CalendarService.ts
@@ -1,34 +1,34 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 export const mock = {
-  name: "Newyear",
+  name: 'Newyear',
 
-  desc: "Diwali or Deepavali is one of the biggest and auspicious festivals celebrated by Hindus all around the globe. The festival of lights signifies peace and joy, the victory of good over evil, and light over darkness every day. It is one of the most symbolic Hindu festivals, and all the communities in the country celebrate it with much pomp. During this festival, people clean their homes, decorate every corner with lights, lamps, diyas, flowers, rangoli, and candles.",
+  desc: 'Diwali or Deepavali is one of the biggest and auspicious festivals celebrated by Hindus all around the globe. The festival of lights signifies peace and joy, the victory of good over evil, and light over darkness every day. It is one of the most symbolic Hindu festivals, and all the communities in the country celebrate it with much pomp. During this festival, people clean their homes, decorate every corner with lights, lamps, diyas, flowers, rangoli, and candles.',
 
-  img: "assets/diwali.jpg",
+  img: 'assets/diwali.jpg',
 
   Uevents: [
     {
-      ename: "Pongal/sankranthi",
-      eimg: "assets/sankranthi.jpg",
-      edate: "14/01/22",
+      ename: 'Pongal/sankranthi',
+      eimg: 'assets/sankranthi.jpg',
+      edate: '14/01/22',
     },
     {
-      ename: "Pongal/sankranthi",
-      eimg: "assets/sankranthi.jpg",
-      edate: "14/01/22",
+      ename: 'Pongal/sankranthi',
+      eimg: 'assets/sankranthi.jpg',
+      edate: '14/01/22',
     },
     {
-      ename: "Pongal/sankranthi",
-      eimg: "assets/sankranthi.jpg",
-      edate: "14/01/22",
+      ename: 'Pongal/sankranthi',
+      eimg: 'assets/sankranthi.jpg',
+      edate: '14/01/22',
     },
   ],
 };
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class CalendarService {
   constructor() {
